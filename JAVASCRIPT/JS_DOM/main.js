56ecca9e80bf2c1eae4fd874db6bc0f033240831
RafaelAkiyama <rafaelakiyama08@gmail.com>
2024-05-20 20:13:28 -0300

    atualizando arquivos

--- JAVASCRIPT/JS_DOM/main.js
+++ JAVASCRIPT/JS_DOM/main.js
@@ -16,3 +16,17 @@ console.log(items[1]);
 items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
+
+
+for (let i = 0; i < items.length; i++) {
+items[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+// Método getElementsByTagName
+let li = document.getElementsByTagName('li');
+console.log(li);
+for (let i = 0; i < li.length; i++) {
+if (i % 2) li[i].style.backgroundColor = '#f4f4f4';
+else li[i].style.backgroundColor = '#fff';
+}
